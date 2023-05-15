28d1147228ddd41c9ba2b076cafc711a662e1f9c
verflou <josuarez@escuelasproa.edu.ar>
2023-05-15 16:32:36 -0300

    el verdaro avance
    esta el de leer,borrar y crear falta modificar

--- controllers/myController.js
+++ controllers/myController.js
@@ -1,21 +1,41 @@
+const { default: mongoose } = require('mongoose');
+const models = require('../models/myModel');
 
-const {default : mongoose} = require('mongoose');
-const models = require("../models/myModel")
 const newmodel = new models({
-    nombre: "joaquin",
-    edad: "17"
+  nombre: 'joaquin',
+  edad: '19',
+});
+
+newmodel
+  .save()
+  .then(() => {
+    console.log('Documento guardado');
+  })
+  .catch((error) => {
+    console.log(error);
   });
 
-newmodel.save()
-.then(() => {
-    console.log("documento se guardo");
-    mongoose.connection.close();
-})
-.catch ((error) =>{
+models
+  .findByIdAndDelete('64628056be2403f643c8dc80')
+  .then((modelEliminado) => {
+    console.log('Documento eliminado correctamente', modelEliminado);
+  })
+  .catch((error) => {
     console.log(error);
-    mongoose.connection.close();
-})
+  });
 
-exports.inicio = (req,res) => {
-    res.status(200).render("index");
-}
+models
+  .find()
+  .then((documentos) => {
+    console.log('Documentos encontrados:', documentos);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => {
+    mongoose.connection.close();
+  });
+
+exports.inicio = (req, res) => {
+  res.status(200).render('index');
+};
