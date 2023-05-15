ccda3848d609d1cb331964a21915618c25b739fd
verflou <josuarez@escuelasproa.edu.ar>
2023-05-15 16:51:36 -0300

    avanceDefinitivo
    le agregamos el metodo de modificar

--- controllers/myController.js
+++ controllers/myController.js
@@ -36,6 +36,17 @@ models
     mongoose.connection.close();
   });
 
+  models.findByIdAndUpdate(('646254a7e30fce64a72378a7'),
+    { nombre: 'Nuevo nombre', edad: '20' },
+    { new: true } // Opcional: devuelve el documento modificado en lugar del original
+  )
+  .then((documentoModificado) => {
+    console.log('646254a7e30fce64a72378a7', documentoModificado);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 exports.inicio = (req, res) => {
   res.status(200).render('index');
 };
